ee095d045686913c1953224507ef7f748df2ff97
maciekb <maciekb@gmail.com>
2016-04-29 01:15:12 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,12 +6,37 @@ if [ ! -d "/var/log/vpnserver" ]; then
   mkdir -p /var/log/vpnserver/server_log
 fi
 
-if [ ! -d "/etc/vpnserver/config" ]; then
-  mkdir -p /etc/vpnserver/config
+if [ -d "/usr/local/vpnserver/backup.vpn_server.config" ]; then
+  if [ ! -L "/usr/local/vpnserver/backup.vpn_server.config" ]; then
+    mkdir -p /etc/vpnserver/config/backup.vpn_server.config
+    mv /usr/local/vpnserver/backup.vpn_server.config /etc/vpnserver/config/
+    ln -s /etc/vpnserver/config/backup.vpn_server.config /usr/local/vpnserver/
+  fi
+else
+  mkdir -p /etc/vpnserver/config/backup.vpn_server.config
+  ln -s /etc/vpnserver/config/backup.vpn_server.config /usr/local/vpnserver/
 fi
 
-mv /usr/local/vpnserver/*.config /etc/vpnserver/config/
-ln -s /etc/vpnserver/config/* /usr/local/vpnserver/
+if [ -f "/usr/local/vpnserver/vpn_server.config" ]; then
+  if [ ! -L "/usr/local/vpnserver/vpn_server.config" ]; then
+    mv /usr/local/vpnserver/vpn_server.config /etc/vpnserver/config/
+    ln -s /etc/vpnserver/config/vpn_server.config /usr/local/vpnserver/vpn_server.config
+  fi
+else
+  touch /etc/vpnserver/config/vpn_server.config
+  ln -s /etc/vpnserver/config/vpn_server.config /usr/local/vpnserver/vpn_server.config
+fi
+
+if [ -f "/usr/local/vpnserver/lang.config" ]; then
+  if [ ! -L "/usr/local/vpnserver/lang.config" ]; then
+    mv /usr/local/vpnserver/lang.config /etc/vpnserver/config/
+    ln -s /etc/vpnserver/config/lang.config /usr/local/vpnserver/lang.config
+  fi
+else
+  touch /etc/vpnserver/config/lang.config
+  ln -s /etc/vpnserver/config/lang.config /usr/local/vpnserver/lang.config
+fi
+
 ln -s /var/log/vpnserver/*_log /usr/local/vpnserver/
 
 exec /usr/local/vpnserver/vpnserver execsvc
