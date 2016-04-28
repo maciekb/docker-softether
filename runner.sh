e93565c184dc4cf25b8fb8a900a6c45c8c38c94a
maciekb <maciekb@gmail.com>
2016-04-29 00:22:04 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
 
-if [ ! -d "/var/log/vpnserver/security_log" ]; then
+if [ ! -d "/var/log/vpnserver" ]; then
   mkdir -p /var/log/vpnserver/security_log
-fi
-
-if [ ! -d "/var/log/vpnserver/packet_log" ]; then
   mkdir -p /var/log/vpnserver/packet_log
-fi
-
-if [ ! -d "/var/log/vpnserver/server_log" ]; then
   mkdir -p /var/log/vpnserver/server_log
+  ln -s /var/log/vpnserver/*_log /usr/local/vpnserver/
 fi
 
-if [ ! -d "/usr/local/vpnserver/config" ]; then
-  mkdir -p /usr/local/vpnserver/config
-  mv /usr/local/vpnserver/vpn_server.conf /usr/local/vpnserver/config/
-  ln -s /usr/local/vpnserver/config/* /usr/local/vpnserver/
+if [ ! -d "/etc/vpnserver/config" ]; then
+  mkdir -p /etc/vpnserver/config
+  mv /usr/local/vpnserver/*.config /etc/vpnserver/config/
+  ln -s /etc/vpnserver/config/* /usr/local/vpnserver/
 fi
-
-ln -s /var/log/vpnserver/*_log /usr/local/vpnserver/
 
 exec /usr/local/vpnserver/vpnserver execsvc
 
